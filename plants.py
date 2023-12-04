0d2218c3471544fa8ab9498a203dcf9a7ed46123
graywes <wdgrayson@outlook.com>
2023-12-04 09:11:05 -0800

    test

--- plants.py
+++ plants.py
@@ -1,3 +1,4 @@
 import requests
 r = requests.get('https://trefle.io/api/v1/genus?token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ')
-r.json
+with open("hi.txt", "w") as f:
+    print(r.json(), file=f)
