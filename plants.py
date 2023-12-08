eeb1b20cf299bea30a5cdf5821fe0d7340d00904
graywes <wdgrayson@outlook.com>
2023-12-08 15:06:47 -0800

    maken

--- plants.py
+++ plants.py
@@ -1,6 +1,6 @@
 import requests
 print(r"""
-Greeting, and welcome to:
+Greetings, and welcome to:
   _____  _             _                  
  |  __ \| |           | |                 
  | |__) | | __ _ _ __ | |_   _____  _____ 
@@ -12,7 +12,10 @@ Greeting, and welcome to:
 
 Please enter command:  
       """)
-text = 
-r = requests.get('https://trefle.io/api/v1/plants/search?token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ&q=coconut')
-with open("hi.txt", "w") as f:
-    print(r.json(), file=f)
+text = input()
+if text == "hi":
+    r = requests.get('https://trefle.io/api/v1/plants/search?token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ&q=coconut')
+    with open("hi.txt", "w") as f:
+        print(r.json(), file=f)
+if text == "search":
+    print()
