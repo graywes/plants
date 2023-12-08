3c5b4bfdf15c14a632274f641c4ddba6591148ea
graywes <wdgrayson@outlook.com>
2023-12-08 14:55:17 -0800

    test

--- plants.py
+++ plants.py
@@ -1,4 +1,18 @@
 import requests
-r = requests.get('https://trefle.io/api/v1/genus?token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ')
+print(r"""
+Greeting, and welcome to:
+  _____  _             _                  
+ |  __ \| |           | |                 
+ | |__) | | __ _ _ __ | |_   _____  _____ 
+ |  ___/| |/ _` | '_ \| __| / _ \ \/ / _ \
+ | |    | | (_| | | | | |_ |  __/>  <  __/
+ |_|    |_|\__,_|_| |_|\__(_)___/_/\_\___|
+      
+        By Wesley Grayson
+
+Please enter command:  
+      """)
+text = 
+r = requests.get('https://trefle.io/api/v1/plants/search?token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ&q=coconut')
 with open("hi.txt", "w") as f:
     print(r.json(), file=f)
