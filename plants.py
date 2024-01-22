a3ce69ce843112655d238caac8364c3b8fe76dac
graywes <wdgrayson@outlook.com>
2024-01-22 15:06:46 -0800

    science

--- plants.py
+++ plants.py
@@ -15,6 +15,8 @@ Greetings, and welcome to:
 Please enter command:  
       """)
 text = input()
+if text == "exit":
+    quit
 if text == "hi":
     r = requests.get('https://trefle.io/api/v1/plants/search?token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ&q=coconut')
     with open("hi.txt", "w") as f:
@@ -28,14 +30,21 @@ if text == "search":
     query = input("enter property to search: ")
     if query == "common name" or query == "1":
         query = input("enter common name: ")
-        search = 'https://trefle.io/api/v1/plants/search?q=' + query + '&token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ'
+        search = 'https://trefle.io/api/v1/plants/search?q=' + query + '&token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ&filter=common_name'
         searched = requests.get(search)
         for j in searched.json().get("data"):
             print(j.get("common_name"))
     if query == "genus" or query == "2":
         query = input("how many pages?: ")
-        for num in range(1, int(query)):
-            search = 'https://trefle.io/api/v1/genus?token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ&page=' + num.__str__()
+        page = input("which page to start?")
+        for num in range(0, int(query)):
+            search = 'https://trefle.io/api/v1/genus?token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ&page=' + (num + int(page)).__str__()
             searched = requests.get(search)
             for j in searched.json().get("data"):
                 print(j.get("name"))
+    if query == "scientific name" or "science" or "3":
+        query = input("enter common name: ")
+        search = 'https://trefle.io/api/v1/plants/search?q=' + query + '&token=Gj6PVBkgY2x-Ix-pSKkrC6Z8g0XgirEHpwSA_Vd6pKQ'
+        searched = requests.get(search)
+        for j in searched.json().get("data"):
+            print(j.get("scientific_name"))
